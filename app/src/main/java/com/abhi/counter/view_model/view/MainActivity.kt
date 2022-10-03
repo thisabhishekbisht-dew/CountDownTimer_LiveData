9f97380762bc94ccfb3bb56ae8c5bfb2d433d212
abhishekb <abhishekb@dewsolutions.in>
2022-10-03 17:27:56 +0530

    Added AppCentre to Project

--- app/src/main/java/com/abhi/counter/view_model/view/MainActivity.kt
+++ app/src/main/java/com/abhi/counter/view_model/view/MainActivity.kt
@@ -7,17 +7,24 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.abhi.counter.view_model.MainActivityViewModel
 import com.abhi.counter_livedata.databinding.ActivityMainBinding
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
+/*Add and Initialize Appcentre to this project*/
+        AppCenter.start(
+            application, "1f3569ef-61f9-4793-bcf4-b0dccccb259c",
+            Analytics::class.java, Crashes::class.java
+        )
         // create instance of the ActivityMainBinding,
         // as we have only one layout activity_main.xml
         val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
-
 
         /*first create instance of viewmodel class*/
         val mainViewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
@@ -46,7 +53,7 @@ class MainActivity : AppCompatActivity() {
             if (it) {
                 binding.tvCounter.text = "Finished"
                 /*make start buttton clickable false*/
-                binding.btnStartAgain.isEnabled=true
+                binding.btnStartAgain.isEnabled = true
             }
         }
 
